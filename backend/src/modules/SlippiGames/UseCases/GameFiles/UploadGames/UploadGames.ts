2c6ee2556fff92a855baa8c711dd0ad3cea60e53
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-08-26 01:49:51 -0500

    Deleting unused UseCase

--- backend/src/modules/SlippiGames/UseCases/GameFiles/UploadGames/UploadGames.ts
+++
@@ -1,9 +0,0 @@
-import { UseCase } from "../../../../../shared/Core/UseCase";
-import { UploadGamesDTO } from "./UploadGamesDTO";
-import { Result } from "../../../../../shared/Core/Result";
-
-export class UploadGames implements UseCase<UploadGamesDTO, Result<void>> {
-    execute(request: UploadGamesDTO): Result<void> {
-        return Result.fail();
-    }
-}
--- backend/src/modules/SlippiGames/UseCases/GameFiles/UploadGames/UploadGamesDTO.ts
+++
@@ -1,3 +0,0 @@
-export interface UploadGamesDTO {
-    gameZipFile: any;
-}
