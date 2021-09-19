beba8cd4f5dc616c88dce7782d665deff88ffb2c
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-09-19 02:45:55 -0500

    Added test for FileParserFactory

--- backend/src/models/File.ts
+++ backend/src/models/File.ts
@@ -3,12 +3,17 @@ import { Result } from "../core/Result";
 interface FileProps {
     data: ArrayBuffer | Buffer;
     extension: string;
+    filename: string;
 }
 
 export class File {
     public static create(props: FileProps): Result<File> {
         if (props.extension === '' || typeof props.extension !== 'string') {
             return Result.fail('Invalid file extension');
+        }
+
+        if(props.filename === '') {
+            return Result.fail('Invalid filename');
         }
 
         return Result.ok(new File(props));
---
+++ backend/src/services/SlippiFileParser/__tests__/FileParserFactory.spec.ts
@@ -0,0 +1,37 @@
+import { FileParserFactory } from '../FileParserFactory';
+import { Mock } from 'moq.ts';
+import { SlippiGameFactory } from '../../../models/SlippiGameFactory';
+import { File } from '../../../models/File';
+import { SupportedFileExtensionEnum } from '../../../models/SupportedFileExtensionEnum';
+import { SlpFileParser } from '../Parsers/SlpFileParser';
+import { ZipFileParser } from '../Parsers/ZipFileParser';
+
+describe('Services: FileParserFactory', () => {
+    test('should return a parser for .slp files', () => {
+        const slippiGameFactory = new Mock<SlippiGameFactory>().object();
+        const parserFactory = new FileParserFactory(slippiGameFactory);
+
+        const fakeBuffer = new Mock<Buffer>().object();
+        const file = new Mock<File>()
+            .setup(file => file.extension)
+            .returns(SupportedFileExtensionEnum.SLP)
+            .object();
+        const parser = parserFactory.makeParser(file);
+
+        expect(parser).toBeInstanceOf(SlpFileParser);
+    });
+
+    test('should return a parser for .zip files', () => {
+        const slippiGameFactory = new Mock<SlippiGameFactory>().object();
+        const parserFactory = new FileParserFactory(slippiGameFactory);
+
+        const fakeBuffer = new Mock<Buffer>().object();
+        const file = new Mock<File>()
+            .setup(file => file.extension)
+            .returns(SupportedFileExtensionEnum.ZIP)
+            .object();
+        const parser = parserFactory.makeParser(file);
+
+        expect(parser).toBeInstanceOf(ZipFileParser);
+    });
+});
